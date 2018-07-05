e37234cac368cdd05396084fb711d562e90e9e51
hColleen <hcolleen@gmail.com>
2018-07-05 02:28:50 -0700

    step 8 ensure URL exists, is defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,17 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+		 
+		it('has URL', function(){
+			for(var i = 0; i < allFeeds.length; i++){
+				expect(allFeeds[i].url).toBeDefined();
+				expect(allFeeds[i].url).not.toBe(null);
+			};
+		});
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
